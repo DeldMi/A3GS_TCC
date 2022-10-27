14b9e74cca70f7407a4e1d0765fa01276a127701
André Felipe <63366610+DeldMi@users.noreply.github.com>
2022-10-27 12:05:42 -0300

    tets

---
+++ web/public/main.js
@@ -0,0 +1,18 @@
+
+var elem, test = "oi"
+
+elem = document.getElementsByTagName('a');
+
+document.getElementById('a').onclick
+
+
+test //?
+
+function getHtml() {
+    var Element = document.getElementById("a");
+    alert(Element.innerHTML);
+    Element.style.color = "orange";
+    Element.innerHTML = "GeeksforGeeks"; //?
+}
+
+
